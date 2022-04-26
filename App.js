b2f95e7c4a5c0e229f1a85fc718ad1c08a6830f4
Anderson Pablo <anderson.pablo02@gmail.com>
2022-04-26 10:15:07 -0300

    feat(screen - map): displaying user map and location

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import IconButton from './components/UI/IconButton';
 import { Colors } from './constants/colors';
+import Map from './screens/Map';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,7 +30,7 @@ export default function App() {
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
-                  size={34}
+                  size={24}
                   color={tintColor}
                   onPress={() => navigation.navigate('AddPlace')}
                 />
@@ -43,6 +44,7 @@ export default function App() {
               title: 'Add a new Place',
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
